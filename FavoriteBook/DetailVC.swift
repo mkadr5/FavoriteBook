f56fa95658f26eb23056c4d5625d808189c03e66
mkadr5 <muhammetkadir52@gmail.com>
2023-03-12 20:49:36 +0300

    Completed

---
+++ FavoriteBook/DetailVC.swift
@@ -0,0 +1,20 @@
+//
+//  DetailVC.swift
+//  FavoriteBook
+//
+//  Created by Muhammet Kadir on 12.03.2023.
+//
+
+import SwiftUI
+
+struct DetailVC: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailVC_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailVC()
+    }
+}
---
+++ FavoriteBook/FavoriteModel.swift
@@ -0,0 +1,8 @@
+//
+//  FavoriteModel.swift
+//  FavoriteBook
+//
+//  Created by Muhammet Kadir on 12.03.2023.
+//
+
+import Foundation

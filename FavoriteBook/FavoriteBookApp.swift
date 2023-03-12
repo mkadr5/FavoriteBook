cb454011ae0e7dfe528ef4f23125ec08dc540791
mkadr5 <muhammetkadir52@gmail.com>
2023-03-12 20:11:09 +0300

    Initial Commit

---
+++ FavoriteBook/FavoriteBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteBookApp.swift
+//  FavoriteBook
+//
+//  Created by Muhammet Kadir on 12.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
